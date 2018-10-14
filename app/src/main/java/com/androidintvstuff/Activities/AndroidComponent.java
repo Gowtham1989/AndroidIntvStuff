7dd9da8fb3fec4f23d6b4fdc94c8ee705507b456
Gowtham1989 <gowthammahadevaiah@gmail.com>
2018-10-14 21:46:05 +0530

    Android Stuff

---
+++ app/src/main/java/com/androidintvstuff/Activities/AndroidComponent.java
@@ -0,0 +1,18 @@
+package com.androidintvstuff.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.androidintvstuff.R;
+
+/**
+ * Created by Gowtham on 10/15/2018.
+ */
+public class AndroidComponent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_component);
+    }
+}
